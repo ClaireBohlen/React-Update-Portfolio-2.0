874d8e9ddc235ac06d4411f2c15e785ab4d207d5
Claire Bohlen <clairebohlen@gmail.com>
2020-11-30 08:51:34 -0700

    style for Footer Wrap

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
-export const FooterContainer = styled.div`
-
+export const FooterContainer = styled.footer`
+    background-color: #101522;
 `
 export const FooterWrap = styled.div`
-
+    padding: 48px 24px;
+    display: flex;
+    flex-direction: colum;
+    justify-content: center;
+    align-items: center;
+    max-width: 1100px;
+    margin: 0 auto;
+    
 `
 export const FooterLinksContainer = styled.div`
 
