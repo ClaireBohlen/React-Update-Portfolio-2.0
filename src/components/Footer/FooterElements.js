112130d473964843f334f3fa4c36ee8257cbc21b
Claire Bohlen <clairebohlen@gmail.com>
2020-11-30 08:48:49 -0700

    adding outline for style in elements

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -1,2 +1,32 @@
 import styled from "styled-components";
 
+export const FooterContainer = styled.div`
+
+`
+export const FooterWrap = styled.div`
+
+`
+export const FooterLinksContainer = styled.div`
+
+`
+export const FooterLinksWrapper = styled.div`
+
+`
+export const FooterLinkItems = styled.div`
+
+`
+export const FooterLinkTitle = styled.div`
+
+`
+export const FooterFooterLink = styled.div`
+
+`
+export const SocailMediaWrap = styled.div`
+
+`
+export const FooterContainer = styled.div`
+
+`
+export const FooterContainer = styled.div`
+
+`
