bb4a73ed1c812dd40b1b79aae9751e66d2767d25
Claire Bohlen <clairebohlen@gmail.com>
2020-11-30 08:52:24 -0700

    styled comps for Links Container

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -11,10 +11,11 @@ export const FooterWrap = styled.div`
     align-items: center;
     max-width: 1100px;
     margin: 0 auto;
-    
+
 `
 export const FooterLinksContainer = styled.div`
-
+    display: flex;
+    justify-content: center;
 `
 export const FooterLinksWrapper = styled.div`
 
