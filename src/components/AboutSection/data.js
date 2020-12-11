ee09dc5f68e98e928c6c3ea6381337d253364156
Claire Bohlen <clairebohlen@gmail.com>
2020-12-11 14:27:31 -0700

    about me

--- src/components/AboutSection/data.js
+++ src/components/AboutSection/data.js
@@ -7,7 +7,7 @@ export const homeObjOne = {
     lightTextDesc: true,
     topLine: "Claire C. Bohlen",
     headline: "About Me:",
-    description: "I'm a full stack software engineer & web developer. I recently graduated from the Unviersity of Colorado with a degree in business & music. I'm currently enrolled in a coding bootcamp at the University of Denver and expect to graduate in December 2020. ",
+    description: "I am a Full Stack Engineer & Developer with a background in Business & Sales.  I recently earned a certificate in Full Stack Development from the University of Denver and I have a bachelor of Science in Business & Music from the University of Colorado.<br>I’m an innovative problem solver that is passionate about developing apps, with a focus on mobile-first design and development. My background in Business Development has prepared me to approach problems creativity and work well with teams.<br>With each project, I aim to understand how to best engage users for an impactful user experience. I applied aspects of UX and agile development in a recent project where I worked in a team of 5 to develop a single-page MERN application.<br>When I’m not designing or developing, I enjoy helping local musicians grow their online presence on social media. Photography, photoshop, video editing & illustrator are some of the skills I put to use when working with musicians. In my free time, I enjoy playing the piano, snowboarding, or reading up on the latest trends in the tech world.<br>I’m eager to leverage my skills in the future as part of a fast-paced, quality-driven team.",
     buttonTarget: "Connect",
     buttonLabel: "Connect with Me!",
     imgStart: false,
