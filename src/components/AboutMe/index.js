cd065d72f9770e19765b40e9e113ab4a5e68a8d9
Claire Bohlen <clairebohlen@gmail.com>
2020-12-12 15:20:05 -0700

    more icons

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -32,7 +32,6 @@ const AboutSection = ({ dark, dark2, primary}) => {
                                 <SubTitle> I am a Full Stack Engineer & Developer with a background in Business & Sales.  I recently earned a certificate in Full Stack Development from the University of Denver and I have a bachelor of Science in Business & Music from the University of Colorado.</SubTitle>
                                 <SubTitle> I’m an innovative problem solver that is passionate about developing apps, with a focus on mobile-first design and development. My background in Business Development has prepared me to approach problems creativity and work well with teams. </SubTitle>
                                 <SubTitle> With each project, I aim to understand how to best engage users for an impactful user experience. I applied aspects of UX and agile development in a recent project where I worked in a team of 5 to develop a single-page MERN application.</SubTitle>
-                                <SubTitle> When I’m not designing or developing, I enjoy helping local musicians grow their online presence on social media. Photography, photoshop, video editing & illustrator are some of the skills I put to use when working with musicians. In my free time, I enjoy playing the piano, snowboarding, or reading up on the latest trends in the tech world.</SubTitle>
                                 
                                 <Heading>I’m eager to leverage my skills in the future as part of a fast-paced, quality-driven team. </Heading>
                                 <BtnWrap>
@@ -51,7 +50,9 @@ const AboutSection = ({ dark, dark2, primary}) => {
                             </TextWrapper>
                         </Colum1>
                         <Colum2>
+                        <SubTitle> When I’m not designing or developing, I enjoy helping local musicians grow their online presence on social media. Photography, photoshop, video editing & illustrator are some of the skills I put to use when working with musicians. In my free time, I enjoy playing the piano, snowboarding, or reading up on the latest trends in the tech world.</SubTitle>
                             <ImgWrap>
+                            
                                 
                                 <Img src={Image}/>
                             </ImgWrap>
